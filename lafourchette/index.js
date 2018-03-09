e15e240fe14d55d4354e523a8e2a3d0a29a4bf14
ljeanner <jeanneret.lucile@gmail.com>
2018-03-09 01:21:38 +0100

    la fourchete

--- lafourchette/index.js
+++ lafourchette/index.js
@@ -1,3 +1,61 @@
 module.exports = function (args) { /* ... */ }
 
-console.log("La fourchette test");
+var request = require("request");
+var cheerio = require("cheerio");
+var fs =require('fs');
+
+
+//var cities [];
+
+function MyRequest(lien){
+    console.log("0");
+request({
+  uri: lien,
+}, function(error,response, body) { 
+  var $ = cheerio.load(body);
+  $(".restaurantResult").each(function() {
+    console.log("1");
+    var link = $(this);
+	var titre=$(this).find(".restaurantResult-name").text().trim();
+      console.log("2");
+    var href = link.attr("href");
+  });  
+}); 
+};
+
+//MyRequest('https://m.lafourchette.com/fr_FR/search?offer=1&sort=POPULARITY&r=50&ll=48.856614,2.3522219000000177&searchText=Paris,%20France&googlePlaceId=ChIJD7fiBh9u5kcRYJSMaMOCCwQ');
+console.log("La fourchette module ici");
+
+function SearchRestaurant(name) {
+    var lien = "https://m.lafourchette.com/fr_FR/search?searchText=" + name;
+    console.log(lien);
+    request({
+  uri: lien,
+}, function(error,response, html) { 
+        console.log("1");
+        var $ = cheerio.load(html);
+         $("restaurantResult-row").each(function() {
+            var titre=$(this).find(".restaurantResult-name").text();
+            console.log(titre);
+            var myObj = {"name":titre};
+            var myJSON = JSON.stringify(myObj);
+            fs.appendFile("lafourchette.json", myJSON, function(err) {
+            if(err) {
+            return console.log(err);
+        }
+    });
+  
+  });  
+}); 
+};
+
+
+SearchRestaurant("Le Chiberta");
+
+var restaurants = [];
+var obj;
+fs.readFile('movies.json', 'utf8', function (err, data) {
+  if (err) throw err;
+  obj = JSON.parse(data);
+});
+
--- michelin/index.js
+++ michelin/index.js
@@ -1,20 +1,101 @@
 var request = require("request");
 var cheerio = require("cheerio");
-console.log("ici");
+var fs =require('fs');
 
+
+/*
+function GetLink(lien,etoile){   
 request({
-  uri: "https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin",
-}, function(error, response, body) {
-
+  uri: lien,
+}, function(error,response, body) {    
   var $ = cheerio.load(body);
-
   $(".poi-card-link").each(function() {
     var link = $(this);
-	var titre=$(this).find(".poi_card-display-title").text();
-	//var ville=$(this).find("").text();
-   // var text = link.text();
     var href = link.attr("href");
+      console.log(href);
+    var url = "https://restaurant.michelin.fr"+href; 
+    GetInfo(url,etoile);
+  });  
+}); 
+};
 
-    console.log("titre " + titre + " -> " + href);
-  });
+function GetInfo(lien,etoile){   
+request({
+  uri: lien,
+}, function(error,response, body) {    
+  var $ = cheerio.load(body);
+  $(".panel-panel ").each(function() {
+    var link = $(this);
+	var titre=$(this).find(".poi_intro-display-title").text().trim();
+      console.log(lien);
+    console.log(titre);
+  
+  });  
+}); 
+};
+*/
+
+function MyRequest(lien,etoile){   
+request({
+  uri: lien,
+}, function(error,response, body) {    
+  var $ = cheerio.load(body);
+  $(".poi-card-link").each(function() {
+      
+    var link = $(this);
+	var titre=$(this).find(".poi_card-display-title").text().trim();
+    var href = link.attr("href");
+    var myObj = {"name":titre,"etoile":etoile,"url":"https://restaurant.michelin.fr"+href};
+    var myJSON = JSON.stringify(myObj);
+      fs.appendFile("michelin.json", myJSON, function(err) {
+        if(err) {
+            return console.log(err);
+        }
+    });
+  
+  });  
+}); 
+};
+
+
+
+/*parcourir */
+
+function Read (){
+fs.open('michelin.json', 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error('michelin does not exist');
+      return;
+    }
+
+    throw err;
+  }
+    
+var contents = fs.readFileSync('michelin.json', 'utf8');
+console.log(contents[10]);
 });
+}
+
+function GetResto (lien, etoile,page)
+{
+    var i=1 ; 
+    while (i<page)
+        {
+        var nv_lien =lien + "/page-"+i;
+        console.log(nv_lien);
+        MyRequest(nv_lien,etoile);
+        i=i+1
+        }  
+}
+
+function Resto_to_Json()
+{
+    
+    GetResto("https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin",1,15);
+    GetResto("https://restaurant.michelin.fr/restaurants/france/restaurants-2-etoile-michelin",2,15);
+    GetResto("https://restaurant.michelin.fr/restaurants/france/restaurants-3-etoile-michelin",3,3);
+};
+
+
+Resto_to_Json();
